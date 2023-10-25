3a013d8eb7f7aa133321a80b63e53089834e3ea7
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-10-25 13:13:21 +0900

    Todoアプリ一覧画面

--- lib/main.dart
+++ lib/main.dart
@@ -21,29 +21,62 @@ class MyTodoApp extends StatelessWidget {
   }
 }
 
+
+
+
+
+
 // リスト一覧画面用Widget
 class TodoListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('リスト一覧画面'),
+      // AppBarを表示し、タイトルも設定
+      appBar: AppBar(
+        title: Text('リスト一覧'),
+      ),
+      // ListViewを使いリスト一覧を表示
+      body: ListView(
+        children: <Widget>[
+          // *** 追加する部分 ***
+          // CardとListTileを使い、簡単に整ったUIを作成
+          Card(
+            child: ListTile(
+              title: Text('ニンジンを買う'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('タマネギを買う'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('ジャガイモを買う'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('カレールーを買う'),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // "push"で新規画面に遷移
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) {
-              // 遷移先の画面としてリスト追加画面を指定
-              return TodoAddPage();
-            }),
-          );
+          // --- 省略 ---
         },
         child: Icon(Icons.add),
       ),
     );
   }
 }
+
+
+
+
+
+
 
 // リスト追加画面用Widget
 class TodoAddPage extends StatelessWidget {
