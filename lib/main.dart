a2344ce05ea3e86c82c24c345d57fc13737aa056
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-10-25 13:08:45 +0900

    Todoアプリ概要

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,37 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Text('リスト一覧画面'),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // "push"で新規画面に遷移
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              // 遷移先の画面としてリスト追加画面を指定
+              return TodoAddPage();
+            }),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+// リスト追加画面用Widget
+class TodoAddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          // ボタンをクリックした時の処理
+          onPressed: () {
+            // "pop"で前の画面に戻る
+            Navigator.of(context).pop();
+          },
+          child: Text('リスト追加画面（クリックで戻る）'),
+        ),
+      ),
     );
   }
 }
